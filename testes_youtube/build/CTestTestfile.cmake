b40bb33f65779c60acadf69ade6a56948ee58017
zurc <afonsobpcruz@gmail.com>
2024-05-14 15:58:51 +0100

    primeiros catkin

---
+++ testes_youtube/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/afonso/Documents/repositorios/SLAM_EKF_Turtlebot3/testes_youtube/src
+# Build directory: /home/afonso/Documents/repositorios/SLAM_EKF_Turtlebot3/testes_youtube/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("tutorials")
---
+++ testes_youtube/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/afonso/Documents/repositorios/SLAM_EKF_Turtlebot3/testes_youtube/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/opt/ros/noetic'.split(';') if '/opt/ros/noetic' != '' else []
